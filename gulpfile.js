76a9bb43de24f5acd6300370fb74492f9fdcbb86
dave <dave.sheffer1@gmail.com>
2017-11-01 10:26:41 +0200

    POST CSS UPDATE

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,11 @@ gulp.task('default', function() {
     console.log("gulpidoo");
           });
 
-gulp.task ('html', function(){
-    
-    console.log("fucking awsome htnl");
-});
 gulp.task ('styles', function(){
     
-    console.log("SASS POST CSS RUNNING");
+    gulp.src('/app/assets/styles/style.css').pipe(gulp.dest('./app/assets/temp/styles'));
 });
 gulp.task ('watch', function(){
-    
-  watch('./app/index.html', function(){
-     gulp.start('html'); 
-  });
      watch('./app/assets/styles/**/*.css', function(){
      gulp.start('styles'); 
   });
