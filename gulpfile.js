92d7eb47bf6aaf7ebc851ae1459612b02ff56bf3
dave <dave.sheffer1@gmail.com>
2017-11-01 12:23:35 +0200

    orginize folders and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    autoprefixer = require('autoprefixer');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     console.log("gulpidoo");
-          });
-
-gulp.task ('styles', function(){
-    
-    gulp.src('/app/assets/styles/style.css').pipe(gulp.dest('./app/assets/temp/styles'));
 });
-gulp.task ('watch', function(){
-     watch('./app/assets/styles/**/*.css', function(){
-     gulp.start('styles'); 
-  });
-    
+
+gulp.task('styles', function () {
+    return gulp.src('./app/assets/styles/style.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/assets/temp/styles'));
+});
+gulp.task('watch', function () {
+    watch('./app/assets/styles/**/*.css', function () {
+        gulp.start('styles');
+    });
+
 });
