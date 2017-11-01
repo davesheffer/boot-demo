71463a46efa5842b2713ef5e12bab0bcfe0807f5
dave <dave.sheffer1@gmail.com>
2017-11-01 15:46:26 +0200

    nearlly there to begin work only thing missing is browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
+    cssImport = require('postcss-import'),
     autoprefixer = require('autoprefixer');
 
 gulp.task('default', function () {
@@ -11,8 +12,8 @@ gulp.task('default', function () {
 
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssvars, nested, autoprefixer]))
-        .pipe(gulp.dest('./app/assets/temp/styles'));
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
 gulp.task('watch', function () {
     watch('./app/assets/styles/**/*.css', function () {
