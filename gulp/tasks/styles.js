abd7c2962b870f33c176a561b2f5d8e3b8569ecf
dave <dave.sheffer1@gmail.com>
2017-11-04 03:32:36 +0200

    gittin with it

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
-    autoprefixer = require('autoprefixer'); 
+    mixins = require('postcss-mixins');
+    ; 
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
         .on('error', function(errorinfo) {
         console.log(errorinfo.toString());
             this.emit('end')
